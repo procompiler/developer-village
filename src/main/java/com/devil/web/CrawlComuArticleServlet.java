81c2b107c697a5737a5a5e5e6666a6d9e427305c
hayeon17kim <qwndhs@gmail.com>
2020-11-25 00:19:38 +0900

    feat: add getRandomNo method for generate random writer

--- src/main/java/com/devil/web/CrawlComuArticleServlet.java
+++ src/main/java/com/devil/web/CrawlComuArticleServlet.java
@@ -3,12 +3,16 @@ package com.devil.web;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import com.devil.domain.Article;
 import com.devil.domain.Comment;
+import com.devil.domain.User;
 import com.devil.service.ArticleService;
 import com.devil.util.Prompt;
 import com.devil.web.filter.Command;
@@ -62,6 +66,8 @@ public class CrawlComuArticleServlet implements Command{
       }
       article.setContent(content.toString());
       article.setCreatedDate(doc.selectFirst("span.timeago").text());
+      User user = new User();
+      //user.setNo(s)
       System.out.println("===========================");
       System.out.println("제목: " + article.getTitle());
       System.out.println("내용: " + article.getContent());
@@ -75,7 +81,7 @@ public class CrawlComuArticleServlet implements Command{
         String commentWriter = li.select("div.avatar-info").select("a.nickname").attr("title");
 
         if (commentWriter.equals(writer)) {
-          System.out.print("작성자: ");
+          comment.setWriter(get)
         } else {
           System.out.print("다른이: ");
         }
@@ -86,68 +92,23 @@ public class CrawlComuArticleServlet implements Command{
       }
       System.out.println("===========================");
 
-      /*
-      Article article = new Article();
-      String title, companyName, date = "";
-      Elements elements = positionDoc.select("table.table-information tbody tr ");
-      title = positionDoc.select("h2.title").text();
-      companyName = positionDoc.select("h4.sub-title").text();
-      article.setTitle(String.format("[%s]%s", companyName, title));
-      System.out.println(article.getTitle());
-
-      for (Element ele : elements) {
-        String raw = ele.text();
-        if (raw.startsWith("기간 ")) {
-          String parsed = raw.replace("기간 ", "");
-          if (raw.contains("까지") && raw.contains("부터")) {
-            parsed = raw.split(" ")[4];
-            date = parsed;
-          } else {
-            date = "2099-12-31";
-          }
-          break;
-        }
-      }
-      article.setEndDate(Date.valueOf(date));
-
-      StringBuilder content = new StringBuilder();
-      Element element = positionDoc.select("div.page-show-detail").select("div.content-body").get(0);
-      Elements sections = new Elements();
-      sections.addAll(element.select("section.section-position"));
-      sections.addAll(element.select("section.section-requirements"));
-      sections.addAll(element.select("section.section-preference"));
-      sections.addAll(element.select("section.section-description"));
-      for (Element section : sections) {
-        String sectionTitle = section.select("h5.section-title").text();
-        if (sectionTitle.length() == 0) {
-          sectionTitle = "회사소개";
-        }
-        content.append("## " + sectionTitle + "\n");
-        // <li> 태그로 묶인 글 add.
-        Elements lis = section.select("li");
-        for (Element li : lis) {
-        	content.append("- " + li.wholeText() + "\n");
-        }
-        if (lis.size() == 0) {
-          // <p> 태그로 묶인 글 add.
-          for (Element p : section.select("p")) {
-            content.append(p.wholeText() + "\n");
-          }
-        }
-        content.append("\n");
-      }
-      article.setContent(content.toString());
-      article.setCategoryNo(3);
-      article.setWriter(2);
-      // add
-      try {
-        articleService.add(article);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-       */
     }
+    
 
+  }
+  public int getRandomNo(int until, int exclusiveNo) {
+	  Random rand = new Random();
+	  while (true) {
+		  int result = rand.nextInt(until);
+		  if (result != exclusiveNo)
+			  return result;
+		  continue;
+	  }
+  }
+  
+  public int getRandomNo(int until) {
+	  Random rand = new Random();
+	  return rand.nextInt(until);
   }
 }
 
