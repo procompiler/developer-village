55d578535ec7300a13b1002ea71f3152f95a5352
jiyounyou <younny418@gmail.com>
2020-11-29 10:07:14 +0900

    feat : CommentDao, CommentService, CommentMapper

--- src/main/java/com/devil/service/DefaultCommentService.java
+++ src/main/java/com/devil/service/DefaultCommentService.java
@@ -1,17 +1,42 @@
 package com.devil.service;
 
+import java.util.List;
 import com.devil.dao.CommentDao;
 import com.devil.domain.Comment;
 
 public class DefaultCommentService implements CommentService {
   CommentDao commentDao;
-  
+
   public DefaultCommentService(CommentDao commentDao) {
     this.commentDao = commentDao;
   }
-  
+
   @Override
   public int add(Comment comment) throws Exception {
     return commentDao.insert(comment);
+  }
+
+  @Override
+  public List<Comment> list(String keyword) throws Exception {
+    return commentDao.findAll(keyword);
+  }
+
+  @Override
+  public Comment get(int no) throws Exception {
+    Comment comment = commentDao.findByNo(no);
+    if (comment != null) {
+      commentDao.updateViewCount(no);
+    }
+    return comment;
+  }
+
+  @Override
+  public int update(Comment comment) throws Exception {
+    return commentDao.update(comment);
+  }
+
+  @Override
+  public int delete(int no) throws Exception {
+    return commentDao.delete(no);
   }
 }
