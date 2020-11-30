2ea7425ce2083e9da7776225bd76e514fb8ded03
hayeon17kim <qwndhs@gmail.com>
2020-12-01 02:45:55 +0900

    feat: enable insertTags in articleAddServlet

--- src/main/java/com/devil/web/ArticleAddServlet.java
+++ src/main/java/com/devil/web/ArticleAddServlet.java
@@ -43,7 +43,7 @@ public class ArticleAddServlet extends HttpServlet {
     out.println("<html>");
     // 웹브라우저 제목에 출력될 내용
     out.println("<head>");
-    out.println("<meta http-equiv='Refresh' content='2;url=list'>");
+    //out.println("<meta http-equiv='Refresh' content='2;url=list'>");
     out.println("<title>게시글 등록</title></head>");
     out.println("<body>");
 
