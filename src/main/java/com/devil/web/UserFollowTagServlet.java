eba2bf53c79520e544c41af4ab9a8e7b2c99dc62
parkminseob <minsub.prk@gmail.com>
2020-12-02 17:23:51 +0900

    feat : userbutton in index.html

--- src/main/java/com/devil/web/UserFollowTagServlet.java
+++ src/main/java/com/devil/web/UserFollowTagServlet.java
@@ -2,7 +2,6 @@ package com.devil.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -10,12 +9,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import com.devil.domain.User;
 import com.devil.service.UserService;
 
 @MultipartConfig(maxFileSize = 1024 * 1024 * 10)
-@WebServlet("/user/updatePhoto")
+@WebServlet("/user/followTag")
 public class UserFollowTagServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
@@ -27,20 +24,18 @@ public class UserFollowTagServlet extends HttpServlet {
     UserService userService =
         (UserService) ctx.getAttribute("userService");
 
-    HttpSession session = request.getSession();
-    User loginUser = session.getAttribute("loginUser");
-    User user = new User();
-    user.setNo(c));
-    user.setNo()
-
-    
-    if (loginUser != null) {
-      .setWriter(loginUser);
-      articleService.add(article);
-      out.println("게시글을 등록했습니다.");
-    } else {
-      out.println("로그인을 해주세요!");
-    }
+    //    User user = new User();
+    //    user.setNo(c));
+    //    user.setNo()
+    //
+    //
+    //    if (loginUser != null) {
+    //      .setWriter(loginUser);
+    //      articleService.add(article);
+    //      out.println("게시글을 등록했습니다.");
+    //    } else {
+    //      out.println("로그인을 해주세요!");
+    //    }
 
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
