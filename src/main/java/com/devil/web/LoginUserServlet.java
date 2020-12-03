dd2ae123ad242ea6d034a880124bc2ad144e9d50
hayeon17kim <qwndhs@gmail.com>
2020-12-04 01:00:19 +0900

    Refactor: seperate loginUser View(JSP) and Controller(Servlet)

--- src/main/java/com/devil/web/LoginUserServlet.java
+++ src/main/java/com/devil/web/LoginUserServlet.java
@@ -1,16 +1,11 @@
 package com.devil.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import com.devil.domain.User;
 
 @WebServlet("/auth/loginUser")
 public class LoginUserServlet extends HttpServlet {
@@ -21,46 +16,7 @@ public class LoginUserServlet extends HttpServlet {
       throws ServletException, IOException {
 
     response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
+    request.getRequestDispatcher("/auth/loginUser.jsp").include(request, response);
 
-    // 클라이언트 전용 보관소(HttpSession)를 준비한다.
-    HttpSession session = request.getSession();
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head><title>로그인 사용자 정보</title></head>");
-    out.println("<body>");
-
-    out.println("<h1>[로그인 사용자 정보]</h1>");
-
-    User user = (User) session.getAttribute("loginUser");
-    if (user == null) {
-      out.println("<p>로그인 하지 않았습니다!</p>");
-      return;
-    }
-    out.printf("<p>번호: %s</p>", user.getNo());
-    out.printf("<p>이메일: %s</p>", user.getEmail());
-    out.printf("<p>닉네임: %s</p>", user.getNickname());
-    out.printf("<p>이름: %s</p>", user.getName());
-
-    DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
-    out.printf("<p>가입일: %s</p>", format.format(user.getCreatedDate()));
-
-    int loginType = Integer.parseInt(user.getLoginType());
-    if(loginType == 1) {
-      out.println("<p>로그인 유형: 기본 가입회원</p>");
-    } else if (loginType == 2) {
-      out.println("<p>로그인 유형: 구글 가입회원</p>");
-    } else if (loginType == 3) {
-      out.println("<p>로그인 유형: 깃허브 가입회원</p>");
-    }
-
-    out.printf("<p>기술 목록: %s</p>", user.getTech());
-    out.printf("<img src='../upload/%s' alt='[%1$s]' height='100px'>", user.getPhoto());
-    out.printf("<p>개인 홈페이지: %s</p>", user.getHomepageURL());
-
-    out.println("<a href='logout'>로그아웃</a>");
-
-    out.println("</body></html>");
   }
 }
--- src/main/webapp/auth/loginUser.jsp
+++ src/main/webapp/auth/loginUser.jsp
@@ -1,12 +1,59 @@
+<%@page import="java.text.SimpleDateFormat"%>
+<%@page import="java.text.DateFormat"%>
+<%@page import="com.devil.domain.User"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+<title>마이페이지</title>
+<link rel="stylesheet" type="text/css" href='../style.css'>
+<link rel="stylesheet"
+  href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+  integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+  crossorigin="anonymous" />
+<style>
+  #urls {
+    padding: 5px;
+    font-size: 2em;
+  }
+</style>
 </head>
-<body>
-
+<body>  
+<jsp:include page="/header.jsp"></jsp:include>
+  
+	<%
+DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+User user = (User) request.getSession().getAttribute("loginUser");
+if (user == null) {
+%>
+	<p>로그인 하지 않았습니다!</p>
+	<%
+  return;
+}
+%>
+	<h1>로그인 사용자 정보</h1>
+	<p>
+		번호<%=user.getNo()%></p>
+	<p>이메일: <%=user.getEmail()%></p>
+	<p>닉네임: <%=user.getNickname()%></p>
+	<p>이름: <%=user.getName()%></p>
+	<p>가입일: <%=format.format(user.getCreatedDate()) %></p>
+	<p>로그인 유형: <%=user.getLoginType().equals("1") ? "기본" : user.getLoginType().equals('2') ? "구글" : "깃허브"%> 가입회원</p>
+	<p>기술 목록: <%=user.getTech() %></p>
+	<img src='../upload/<%=user.getPhoto()%>.jpg' alt='[<%=user.getPhoto()%>.jpg]' height='100px'>
+	<div id="urls">
+	<% if (user.getHomepageURL() != null) {%>
+	 <a href="<%=user.getHomepageURL()%>"><i class="fas fa-home"></i></a>
+	<% } if (user.getGithubURL() != null) {%>
+	 <a href="<%=user.getGithubURL()%>"><i class="fab fa-github-alt"></i></a>
+	<% } if (user.getInstarURL() != null) {%>
+	<a href="<%=user.getInstarURL()%>"><i class="fab fa-instagram"></i></i></a>
+	<% } if (user.getTwitterURL() != null) {%>
+	 <a href="<%=user.getTwitterURL()%>"><i class="fab fa-twitter"></i></i></a>
+	<% } %>
+	</div>
+	<a href='logout'>로그아웃</a>
+	<jsp:include page="/footer.jsp"></jsp:include>
 </body>
 </html>
