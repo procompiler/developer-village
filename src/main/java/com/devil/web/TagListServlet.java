d6bd2350cbf4750b3c665693bf745761c19a3bc7
cchoijjinyoung <cgy7127@naver.com>
2020-12-03 21:08:51 +0900

    feat : tagList.jsp

--- src/main/java/com/devil/web/TagListServlet.java
+++ src/main/java/com/devil/web/TagListServlet.java
@@ -1,8 +1,6 @@
 package com.devil.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -24,66 +22,20 @@ public class TagListServlet extends HttpServlet {
 
     ServletContext ctx = request.getServletContext();
     TagService tagService = (TagService) ctx.getAttribute("tagService");
-    
+
     User loginUser = (User) request.getSession().getAttribute("loginUser");
 
 
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head><title>태그목록</title>");
-    out.println("<link rel=\"stylesheet\" type=\"text/css\" href='../style.css'></head>");
-    out.println("<body>");
-
     try {
-      out.println("<h1>태그 목록</h1>");
-      out.println("<button type='button' onclick=\"location.href='form.html'\">태그 추가</button>");
-
       List<Tag> list = tagService.list(null);
-      out.println("<table border='1'>");
-      out.println("<thead>");
-      out.println("<tr>" // table row
-          + "<th>번호</th>" // table header
-          + "<th>태그이름</th>" + "<th>태그사진</th>" + "<th>미리보기</th>" + "<th>삭제여부</th>" + "<th></th></tr>");
-      out.println("</thead>");
-      out.println("<tbody>");
-      List<Integer> tagNoList = new ArrayList<>();
-      for (Tag t : loginUser.getTags()) {
-        tagNoList.add(t.getNo());
-      }
-      for (Tag tag : list) {
-        boolean followed = tagNoList.contains(tag.getNo());
-        out.printf(
-            "<tr>"
-        + "<td>%d</td>"
-        + "<td id=\"title\"><a href='detail?no=%1$d'>%s</a></td>"
-        + "<td><img src='../upload/tag/%s_80x80.png' alt='%3$s'></td>"
-        + "<td><span id=\"color\" style=\"background-color:#%s; color:#%s\">%2$s</span></td>"
-        + "<td>%s</td>"
-        + "<td><button type='button' %s onclick=\"location.href='../user/%sfollowTag?tno=%1$d'\">%s</button></td>"
-        + "</tr>\n",
-            tag.getNo(),
-            tag.getName(),
-            tag.getPhoto(),
-            tag.getTagColor(),
-            tag.getFontColor(),
-            tag.getState() == 1 ? "" : "삭제됨",
-            followed ? "class='btn-hollow'" : "",
-            followed ? "un" : "",
-            followed ? "언팔로우" : "팔로우");
-      }
-      
-      out.println("</tbody>");
-      out.println("</table>");
+      request.setAttribute("list", list);
+      request.getRequestDispatcher("/tag/list.jsp").include(request, response);
 
     } catch (Exception e) {
       request.setAttribute("exception", e);
       request.getRequestDispatcher("/error").forward(request, response);
       return;
     }
-    out.println("</body>");
-    out.println("</html>");
   }
 
 }
