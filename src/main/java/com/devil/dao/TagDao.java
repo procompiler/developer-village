46393ef5e4fac530aeced3110c9988b3622790f8
hayeon17kim <qwndhs@gmail.com>
2020-12-04 12:41:53 +0900

    refactor to use tagServlet in loginUser

--- src/main/java/com/devil/dao/TagDao.java
+++ src/main/java/com/devil/dao/TagDao.java
@@ -2,6 +2,7 @@ package com.devil.dao;
 
 import java.util.List;
 import com.devil.domain.Tag;
+import com.devil.domain.User;
 
 public interface TagDao {
   int insert(Tag tag) throws Exception;
@@ -10,5 +11,5 @@ public interface TagDao {
   int delete(int no) throws Exception;
   Tag findByNo(int no) throws Exception;
   int inactive(int no) throws Exception;
-  List<Tag> findByFollower(int userNo) throws Exception;
+  List<Tag> findByFollower(User user) throws Exception;
 }
