e8573146ac2a7120124a971447d2d2d5b2ea15d8
jiyounyou <younny418@gmail.com>
2020-12-16 17:01:49 +0900

    string-mybatis association complete

--- src/main/java/com/devil/dao/BlockDao.java
+++ src/main/java/com/devil/dao/BlockDao.java
@@ -5,7 +5,7 @@ import com.devil.domain.Block;
 
 public interface BlockDao {
   int insert(Block block) throws Exception;
-  int update(Block block) throws Exception;
+  int update(Block block) throws Exception; // Mapper에 구현 안함
   List<Block> findAll(String keyword) throws Exception;
   Block findByNo(int no) throws Exception;
   Block findByUserNo(int no) throws Exception;
--- src/main/java/com/devil/dao/UserDao.java
+++ src/main/java/com/devil/dao/UserDao.java
@@ -6,11 +6,11 @@ import com.devil.domain.User;
 
 public interface UserDao {
   int insert(User user) throws Exception;
+  int update(User user) throws Exception;
   int delete(int no) throws Exception;
   User findByNo(int no) throws Exception;
   List<User> findByName(String name) throws Exception;
   List<User> findAll(String keyword) throws Exception;
-  int update(User user) throws Exception;
   User findByEmailPassword(String email, String password) throws Exception;
   int inactive(int no) throws Exception;
   List<User> findByFollower(User user) throws Exception;
